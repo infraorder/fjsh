db7609b67802f959b0f24059321352acc447ba6d
order <nathanielstewartmcintosh@gmail.com>
2022-01-20 19:51:17 +0200

    testing

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   mode: 'production',
-  entry: './src/fjsh.ts',
+  entry: './src/index.ts',
   output: {
     path: path.resolve(__dirname, '../dist/umd'),
     filename: 'index.js',
